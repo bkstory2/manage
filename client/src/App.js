fe5ed89b98858ceadee458ee83aab7ce1b0ecb96
bkstory2 <bkstory@nate.com>
2025-04-01 01:09:09 +0900

    app

--- client/src/App.js
+++ client/src/App.js
@@ -41,6 +41,8 @@ function App() {
             <TableCell>생일</TableCell>
             <TableCell>성별</TableCell>
             <TableCell>직업</TableCell>
+
+            
           </TableRow>
         </TableHead>
         <TableBody>
